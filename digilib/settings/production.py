761e8c71ac991d020de815d19bc2fa0d8afde2b8
John J Lawal <jophilip79@gmail.com>
2025-04-19 06:34:25 +0100

    Updates secret key environment variable name
    Updates the environment variable name used to retrieve the secret key from `DJANGO_SECRET_KEY` to `SECRET_KEY`.
    This ensures consistency and proper loading of the secret key in the production environment.

--- digilib/settings/production.py
+++ digilib/settings/production.py
@@ -5,7 +5,7 @@ import os
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
